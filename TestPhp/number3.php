c9a16fe965075618c04f5b904cf0b5bcf6cb852e
Kenji <120458555+schoolkenji@users.noreply.github.com>
2024-02-17 22:18:11 +0800

    added lab exam

--- TestPhp/number3.php
+++ TestPhp/number3.php
@@ -8,7 +8,7 @@ $n3_preferences = array("theme" => "light", "language" => "en_US");
 
 $n3_encoded = json_encode($n3_preferences);
 
-setcookie($n3_cookie_name, $n3_cookie_value, $n3_user_id, $n3_token, $n3_encoded, time() + (60*60*24*30), "/");
+setcookie($n3_cookie_name, $n3_cookie_value, $n3_user_id, $n3_token, $n3_encoded, time() + (60*60*24*30));
 
 if (isset($_COOKIE[$n3_cookie_name])) {
     echo "Cookie $n3_cookie_name is set! Value is $n3_cookie_value";
